cb333ea4ef260157ecb770f4838473b46a7f26a0
Doubleap <andres.aymerich@gmail.com>
2019-09-05 01:19:08 -0600

    Busqueda general en mant clientes.

--- app/src/main/java/proyecto/app/clientesabc/actividades/MantClienteActivity.java
+++ app/src/main/java/proyecto/app/clientesabc/actividades/MantClienteActivity.java
@@ -1,5 +1,6 @@
 package proyecto.app.clientesabc.actividades;
 
+import android.app.SearchManager;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.drawable.ColorDrawable;
@@ -9,19 +10,22 @@ import android.graphics.drawable.ShapeDrawable;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.PopupMenu;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
 import android.view.ContextThemeWrapper;
 import android.view.Gravity;
 import android.view.LayoutInflater;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -29,12 +33,14 @@ import android.widget.Toast;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-import proyecto.app.clientesabc.adaptadores.DataBaseHelper;
 import proyecto.app.clientesabc.R;
+import proyecto.app.clientesabc.adaptadores.DataBaseHelper;
 
 
 public class MantClienteActivity extends AppCompatActivity {
     Intent intent;
+    private SearchView searchView;
+    private MyAdapter mAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,7 +49,7 @@ public class MantClienteActivity extends AppCompatActivity {
         ArrayList<HashMap<String, String>> clientList = db.getClientes();
         RecyclerView rv = findViewById(R.id.user_list);
 
-        MyAdapter mAdapter = new MyAdapter(clientList);
+        mAdapter = new MyAdapter(clientList);
         rv.setLayoutManager(new LinearLayoutManager(this));
         rv.setAdapter(mAdapter);
         rv.addItemDecoration(new DividerItemDecoration(this.getBaseContext(), DividerItemDecoration.VERTICAL));
@@ -52,14 +58,53 @@ public class MantClienteActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Un SnackBar", Snackbar.LENGTH_LONG)
-                        .setAction("Accion", null).show();
+                Bundle b = new Bundle();
+                //TODO seleccionar el tipo de solicitud por el UI
+                b.putString("tipoSolicitud", "1"); //id de solicitud
+                Intent intent = new Intent(view.getContext(),SolicitudActivity.class);
+                intent.putExtras(b); //Pase el parametro el Intent
+                startActivity(intent);
             }
         });
     }
 
-    public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_search, menu);
+
+        // Associate searchable configuration with the SearchView
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        searchView = (SearchView) menu.findItem(R.id.action_search).getActionView();
+        if (searchView != null) {
+            if (searchManager != null) {
+                searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+            }
+            searchView.setMaxWidth(Integer.MAX_VALUE);
+            searchView.setQueryHint("Buscar...");
+
+            // listener de buscar query text change
+            searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+                @Override
+                public boolean onQueryTextSubmit(String query) {
+                    // filter recycler view when query submitted
+                    mAdapter.getFilter().filter(query);
+                    return false;
+                }
+
+                @Override
+                public boolean onQueryTextChange(String query) {
+                    // filter recycler view when text is changed
+                    mAdapter.getFilter().filter(query);
+                    return false;
+                }
+            });
+        }
+        return true;
+    }
+
+    public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder>  implements Filterable {
         private ArrayList<HashMap<String, String>> mDataset;
+        private ArrayList<HashMap<String, String>> formListFiltered;
         // Provide a reference to the views for each data item
         // Complex data items may need more than one view per item, and
         // you provide access to all the views for a data item in a view holder
@@ -75,6 +120,7 @@ public class MantClienteActivity extends AppCompatActivity {
         // Constructor de Adaptador HashMap
         private MyAdapter(ArrayList<HashMap<String, String>> myDataset) {
             mDataset = myDataset;
+            formListFiltered = mDataset;
         }
 
         // Crear nuevas Views
@@ -92,9 +138,9 @@ public class MantClienteActivity extends AppCompatActivity {
             // - Obtener Elemento del data set en esta position
             // - Reemplazar aqui cualquier contenido dinamico dependiendo de algun valor de l dataset creado y o el contenido del dataset
             TextView codigo = holder.listView.findViewById(R.id.textViewHead);
-            codigo.setText(mDataset.get(position).get("codigo"));
+            codigo.setText(formListFiltered.get(position).get("codigo"));
             TextView nombre = holder.listView.findViewById(R.id.textViewDesc);
-            nombre.setText(mDataset.get(position).get("nombre"));
+            nombre.setText(formListFiltered.get(position).get("nombre"));
             TextView textViewOptions = holder.listView.findViewById(R.id.textViewOptions);
             ImageView estado = holder.listView.findViewById(R.id.estado);
             Drawable d = getResources().getDrawable(R.drawable.circulo_status_cliente, null);
@@ -166,7 +212,40 @@ public class MantClienteActivity extends AppCompatActivity {
         // Return the size of your dataset (invoked by the layout manager)
         @Override
         public int getItemCount() {
-            return mDataset.size();
+            return formListFiltered.size();
+        }
+
+        //Para filtrar busquedas segun criterios
+        @Override
+        public Filter getFilter() {
+            return new Filter() {
+                @Override
+                protected FilterResults performFiltering(CharSequence charSequence) {
+                    String charString = charSequence.toString();
+                    if (charString.isEmpty()) {
+                        formListFiltered = mDataset;
+                    } else {
+                        ArrayList<HashMap<String, String>> filteredList = new ArrayList<>();
+                        for (HashMap<String, String> row : mDataset) {
+                            if (row.get("codigo").trim().contains(charString) || row.get("nombre").toUpperCase().trim().contains(charString.toUpperCase()) || row.get("direccion").trim().contains(charString)) {
+                                filteredList.add(row);
+                            }
+                        }
+                        formListFiltered = filteredList;
+                    }
+
+                    FilterResults filterResults = new FilterResults();
+                    filterResults.values = formListFiltered;
+                    return filterResults;
+                }
+                @SuppressWarnings("unchecked")
+                @Override
+                protected void publishResults(CharSequence charSequence, FilterResults filterResults) {
+                    formListFiltered = (ArrayList<HashMap<String, String>>) filterResults.values;
+                    // refresh the list with filtered data
+                    notifyDataSetChanged();
+                }
+            };
         }
     }
 
