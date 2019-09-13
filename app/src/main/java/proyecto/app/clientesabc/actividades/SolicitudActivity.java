8d649e40c004b1a93e82634f917d46b9d94f0f5c
Doubleap <andres.aymerich@gmail.com>
2019-09-13 07:28:17 -0600

    Intento de reduccion de tamano de imagenes adjuntadas, sin perder mucha calidad de imagen

--- app/src/main/java/proyecto/app/clientesabc/actividades/SolicitudActivity.java
+++ app/src/main/java/proyecto/app/clientesabc/actividades/SolicitudActivity.java
@@ -19,6 +19,7 @@ import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Environment;
 import android.preference.PreferenceManager;
 import android.provider.MediaStore;
 import android.provider.OpenableColumns;
@@ -65,7 +66,10 @@ import android.widget.Toast;
 import com.vicmikhailau.maskededittext.MaskedEditText;
 
 import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.lang.ref.WeakReference;
@@ -561,8 +565,28 @@ public class SolicitudActivity extends AppCompatActivity {
                         String type = cR.getType(uri);
                         String name = getFileName(cR, uri);
                         byte[] inputData = getBytes(iStream);
+                        try {
+                            File file = new File( Environment.getExternalStorageDirectory().getAbsolutePath());
+                            if (!file.exists()) {
+                                file.createNewFile();
+                            }
+                            FileOutputStream fos = new FileOutputStream(file+"\\"+name);
+                            fos.write(inputData);
+                            fos.close();
+                        } catch (Exception e) {
+                            Log.e("thumbnail", e.getMessage());
+                        }
+                        File file = new File( Environment.getExternalStorageDirectory().getAbsolutePath()+"\\"+name);
+                        saveBitmapToFile(file);
+
+                        byte[] bytesArray = new byte[(int) file.length()];
+
+                        FileInputStream fis = new FileInputStream(file);
+                        fis.read(bytesArray); //read file into bytes[]
+                        fis.close();
+
                         //Agregar al tableView del UI
-                        Adjuntos nuevoAdjunto = new Adjuntos(GUID, type, name, inputData);
+                        Adjuntos nuevoAdjunto = new Adjuntos(GUID, type, name, bytesArray);
 
                         adjuntosSolicitud.add(nuevoAdjunto);
                         AdjuntoTableAdapter stda = new AdjuntoTableAdapter(getBaseContext(), adjuntosSolicitud);
@@ -3062,5 +3086,49 @@ public class SolicitudActivity extends AppCompatActivity {
         }
 
     }
+
+    public File saveBitmapToFile(File file){
+        try {
+
+            // BitmapFactory options to downsize the image
+            BitmapFactory.Options o = new BitmapFactory.Options();
+            o.inJustDecodeBounds = true;
+            o.inSampleSize = 6;
+            // factor of downsizing the image
+
+            FileInputStream inputStream = new FileInputStream(file);
+            //Bitmap selectedBitmap = null;
+            BitmapFactory.decodeStream(inputStream, null, o);
+            inputStream.close();
+
+            // The new size we want to scale to
+            final int REQUIRED_SIZE=500;
+
+            // Find the correct scale value. It should be the power of 2.
+            int scale = 1;
+            while(o.outWidth / scale / 2 >= REQUIRED_SIZE &&
+                    o.outHeight / scale / 2 >= REQUIRED_SIZE) {
+                scale *= 2;
+            }
+
+            BitmapFactory.Options o2 = new BitmapFactory.Options();
+            o2.inSampleSize = scale;
+            inputStream = new FileInputStream(file);
+
+            Bitmap selectedBitmap = BitmapFactory.decodeStream(inputStream, null, o2);
+            inputStream.close();
+
+            // here i override the original image file
+            file.createNewFile();
+            FileOutputStream outputStream = new FileOutputStream(file);
+
+            selectedBitmap.compress(Bitmap.CompressFormat.JPEG, 100 , outputStream);
+
+            return file;
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
 
 }
